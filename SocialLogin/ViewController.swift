5e51b77072b2ebf07b7d88ac6ecfacc24d51a594
miteshAnnex <mpatel@annexcloud.com>
2024-03-03 12:50:12 +0530

    Initial Commit

---
+++ SocialLogin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLogin
+//
+//  Created by MiteshKumar Patel on 03/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
