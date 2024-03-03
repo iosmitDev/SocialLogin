55e6d0836828b99b78f78c4f9ebe51ec1fdbed46
iosmitDev <136920248+iosmitDev@users.noreply.github.com>
2024-03-03 21:52:14 +0530

    Update ViewController File test 2

--- SocialLogin/ViewController.swift
+++ SocialLogin/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("test1")
+        print("test2")
        print("New1")
         addition(5, 7)
     }
